67fad8696a5bddf2fb8998cb10ecf88fb844c013
Daniela Noir <daniela@danielanoir.com>
2016-07-13 18:23:23 -0400

    almost done. It's all functional, just need to add details. Did not figure out how to remove dino name from left hand side after clicking button.

--- script.js
+++ script.js
@@ -1,69 +1,219 @@
 $(document).ready(function(){
-console.log("JQuery is working.");
-
-// first variable dinoNamesList is being pulled from HTML
-// Created string of dino names instead of just referencing from HTML so I can append from JS.
-var dinoNamesList = $("ul.dinoNamesNotLearned li")
-var dinoNamesForEdits = ["Tyrannosaurus rex", "Velociraptor", "Allosaurus", "Spinosaurus", "Deinonychus", "Carnotaurus", "Apatosaurus", "Diplodocus", "Brachiosaurus", "Stegosaurus", "Ankylosaurus", "Triceratops", "Iguandon", "Parasaurolophus"]
+  console.log("JQuery is working.");
 
 
-console.log(dinoNamesForEdits);
-//created a for loop. var i (i.e instance) is the starting point of the for loop.
-// i < dinoNames.length defins the conditions for the for loop (i must be less than the length of the dinoNames)
-//dinoNames.length needs to be more than the instance
-// Increase value (i++) each time code block in the loop has been executed.
-//dinoNames.eq(i) is basically calling the dinoNames. It's equivelant to dinoNames[]
-for (var i = 0; i < dinoNamesForEdits.length; i++) {
-//need to add the string so that it runs the names and not just the number.
-  console.log(dinoNamesForEdits[i]);
-}
+  $(".dinoNamesNotLearned").css({"width":"275px","font-size":"30px","font-family":"'Comic Sans MS', cursive, sans-serif","background":"rgba(18, 27, 38, 0.4)"});
+  $(".dinoNamesLearned").css({"font-size":"30px","font-family":"'Comic Sans MS', cursive, sans-serif","background":"rgba(18, 27, 38, 0.4)"});
+  $(".memorizedButton").css({"border-style":"outset","font-weight":"bold","color":"black","font-size":"40px","font-family":"'Comic Sans MS', cursive, sans-serif","background-color":"#ddbd3e","border-color":"grey","margin":"20px","padding":"10px"});
 
+//dinoDetails are objects in array with details on each of the dinos. Refering to it in if else.
+//Dynamic data goes below for each card. Static is in HTML
+  var dinoCards = [
+    {
+      name: "Tyrannosaurus rex",
+      question: "What is the correct answer?",
+      correctAnswer: "A",
+      options:[],
+      img: "trex.jpg"
+    },
+    {
+      name: "Velociraptor",
+      question: "What is the correct answer?",
+      correctAnswer: "D",
+      img: "velociraptor.jpg"
+    },
+    {
+      name: "Allosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "C",
+      img: "allosaurus.jpg"
+    },
+    {
+      name: "Spinosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "C",
+      img: "spinosaurus.jpg"
+    },
+    {
+      name: "Deinonychus",
+      question: "What is the correct answer?",
+      correctAnswer: "A",
+      img: "deinonychus.jpg"
+    },
+    {
+      name: "Apatosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "D",
+      img: "apatosaurus.jpg"
+    },
+    {
+      name: "Diplodocus",
+      question: "What is the correct answer?",
+      correctAnswer: "C",
+      img: "diplodocus.jpg"
+    },
+    {
+      name: "Brachiosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "B",
+      img: "brachiosaurus.jpg"
+    },
+    {
+      name: "Stegosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "A",
+      img: "stegosaurus.jpg"
+    },
+    {
+      name: "Ankylosaurus",
+      question: "What is the correct answer?",
+      correctAnswer: "B",
+      img: "ankylosaurus.jpg"
+    },
+    {
+      name: "Triceratops",
+      question: "What is the correct answer?",
+      correctAnswer: "B",
+      img: "triceratops.jpg"
+    },
+    {
+      name: "Iguandon",
+      question: "What is the correct answer?",
+      correctAnswer: "C",
+      img: "iguandon.jpg"
+    },
+    {
+      name: "Parasaurolophus",
+      question: "What is the correct answer?",
+      correctAnswer: "D",
+      img: "parasaurolophus.jpg"
+    },
 
+  ]
 
-$(".cardBack").hide();
+  //created a for loop. var i (i.e instance) is the starting point of the for loop.
+  // i < dinoNames.length defins the conditions for the for loop (i must be less than the length of the dinoNames)
+  //dinoNames.length needs to be more than the instance
+  // Increase value (i++) each time code block in the loop has been executed.
+  // added the array of dino names to the dino names not learned section using append, and then added a space in between each.
 
-$( ".multipleChoice" ).click(function() {
-  var input = prompt("please type A or B");
-  if (input == "A"){
-    alert("You got it!");
-    $(".cardBack").show();
-    $(".flashCard").hide();
-    //flip card to show details
-  } else {
-    alert("Sorry, that is incorrect.");
-    $(".cardBack").show();
-    $(".flashCard").hide();
-    //flip card to show details
+  for (var i = 0; i < dinoCards.length; i++) {
+    // set the data-dino-index to the same value taht we are looping through. i represents incrementing value in loop.
+    var paraTags = $("<p class='dinosaur' data-dino-index='" + i + "'></p>")
+    paraTags.append(dinoCards[i].name)
+    $(".dinoNamesNotLearned").append(paraTags)
+    //$(this) is selecting the p tags, which is the object the event listener is attached to.
+    // paraTags.click(function () {
+    //   console.log(dinosaur.name);
+    //   console.log(i + "?");
+    //   changeCard(dinosaur)
+    //   console.log($(this).html());
+    //  $(this).detach();
+    //  $(".dinoNamesLearned").append($(this))
+    // })
   }
 
+//created function that clicks on the name of the dinosaur
+  $("p.dinosaur").click(function (){
+    console.log("dino clicked")
+    var dinoIndex = $(this).attr("data-dino-index")
+    var dinoCard = dinoCards[dinoIndex]
+    changeCard(dinoCard)
+  })
+  //adding the object to the card front and back so that it shows info:
+  function changeCard(dinoCard) {
+    console.log(dinoCard)
+    $(".cardFront").html("<h1 class='heading'>"+ dinoCard.name +"</h1>" + "<p class='question'>" + dinoCard.question + "</p>" + "<img class='animals' src='" + dinoCard.img + "'>")
+    $(".cardBack").html("<h1 class='answer'>"+ dinoCard.correctAnswer +"</h1>")
+  }
+
+
+
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[0].name) {
+      $(".dinoNamesLearned").append("<p>Tyrannosaurus rex</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[1].name) {
+      $(".dinoNamesLearned").append("<p>Velociraptor</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[2].name) {
+      $(".dinoNamesLearned").append("<p>Allosaurus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[3].name) {
+      $(".dinoNamesLearned").append("<p>Spinosaurus</p>")
+      $("dinosaur").attr("data-dino-index"[0]).removeAttr();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[4].name) {
+      $(".dinoNamesLearned").append("<p>Deinonychus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[5].name) {
+      $(".dinoNamesLearned").append("<p>Apatosaurus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[6].name) {
+      $(".dinoNamesLearned").append("<p>Diplodocus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[7].name) {
+      $(".dinoNamesLearned").append("<p>Brachiosaurus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[8].name) {
+      $(".dinoNamesLearned").append("<p>Stegosaurus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[9].name) {
+      $(".dinoNamesLearned").append("<p>Ankylosaurus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[10].name) {
+      $(".dinoNamesLearned").append("<p>Triceratops</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[11].name) {
+      $(".dinoNamesLearned").append("<p>Iguandon</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+  $(".memorizedButton").click(function() {
+    if ($(".cardFront h1").text() === dinoCards[12].name) {
+      $(".dinoNamesLearned").append("<p>Parasaurolophus</p>")
+      $("dinosaur").attr("data-dino-index").remove();
+    }
+  })
+
+//function created to flip the card and shw the answer:
+$(document).ready(function() {
+  $('.flashCard').on('click', function() {
+    $('.flashCard').toggleClass('flipped');
+  });
 });
 
 
-
 });
-
- // create array of strings, each a dino name, so that I can write HTML from javascript (so we can append it to the html).
-//  var allTheDinos = ["Tyrannosaurus rex", "Velociraptor", "Allosaurus", "Spinosaurus", "Deinonychus", "Carnotaurus", "Apatosaurus", "Diplodocus", "Brachiosaurus", "Stegosaurus", "Ankylosaurus", "Triceratops", "Iguandon", "Parasaurolophus"];
-// console.log(allTheDinos[0]);
-
-// $(".dinoNamesNotLearned").append(["Tyrannosaurus rex", "Velociraptor", "Allosaurus", "Spinosaurus", "Deinonychus,", "Carnotaurus", "Apatosaurus", "Diplodocus", "Brachiosaurus", "Stegosaurus", "Ankylosaurus", "Triceratops", "Iguandon", "Parasaurolophus"]);
-
-  //first, make it so the for loop posts the flash card with the dinasaurs name
-  //create for loop within each arrays string that goes through each multiple choice
-
-
-
-// PSUEDO:
-//
-// All dino names are in learning section to start as arrays
-// use for loops to go through array.
-// create 4 buttons on each flashcard with multiple choice answers onclick.
-// If user clicks on correct button, alert saying "You got it!"
-//   then, card flips to show name and details of dino
-//        need to have a card back
-//        also, name of dino goes to memorized section
-// Else user selects wrong answer, alert saying "Sorry, try again"
-//   then, card flips to show name and details of dino
-//   also, name of dino stays in learning section
-//
-//
