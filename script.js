3d5bfff179ddee523b6dd1e77aa22571ae0f7776
Daniela Noir <daniela@danielanoir.com>
2016-07-13 22:23:53 -0400

    cleaned up project. Ready to submit.

--- script.js
+++ script.js
@@ -101,118 +101,34 @@ $(document).ready(function(){
     var paraTags = $("<p class='dinosaur' data-dino-index='" + i + "'></p>")
     paraTags.append(dinoCards[i].name)
     $(".dinoNamesNotLearned").append(paraTags)
-    //$(this) is selecting the p tags, which is the object the event listener is attached to.
-    // paraTags.click(function () {
-    //   console.log(dinosaur.name);
-    //   console.log(i + "?");
-    //   changeCard(dinosaur)
-    //   console.log($(this).html());
-    //  $(this).detach();
-    //  $(".dinoNamesLearned").append($(this))
-    // })
   }
+
+  //adding the object to the card front and back so that it shows info:
+  function changeCard(IthefunctionExpectParameter) {
+    $(".cardFront").html("<h1 class='heading'>"+ IthefunctionExpectParameter.name +"</h1>" + "<p class='question'>" + IthefunctionExpectParameter.question + "</p>" + "<img class='animals' src='" + IthefunctionExpectParameter.img + "'>" + "<p>Click card to learn...</p>")
+    $(".cardBack").html("<h1 class='answer'>"+ IthefunctionExpectParameter.correctAnswer +"</h1>")
+  }
+
+var dinoIndex = new Number;
 
 //created function that clicks on the name of the dinosaur
   $("p.dinosaur").click(function (){
     console.log("dino clicked")
-    var dinoIndex = $(this).attr("data-dino-index")
-    var dinoCard = dinoCards[dinoIndex]
-    changeCard(dinoCard)
-  })
-  //adding the object to the card front and back so that it shows info:
-  function changeCard(dinoCard) {
-    console.log(dinoCard)
-    $(".cardFront").html("<h1 class='heading'>"+ dinoCard.name +"</h1>" + "<p class='question'>" + dinoCard.question + "</p>" + "<img class='animals' src='" + dinoCard.img + "'>")
-    $(".cardBack").html("<h1 class='answer'>"+ dinoCard.correctAnswer +"</h1>")
-  }
-
-
-
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[0].name) {
-      $(".dinoNamesLearned").append("<p>Tyrannosaurus Rex</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[1].name) {
-      $(".dinoNamesLearned").append("<p>Velociraptor</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[2].name) {
-      $(".dinoNamesLearned").append("<p>Allosaurus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[3].name) {
-      $(".dinoNamesLearned").append("<p>Spinosaurus</p>")
-      $("dinosaur").attr("data-dino-index"[0]).removeAttr();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[4].name) {
-      $(".dinoNamesLearned").append("<p>Deinonychus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[5].name) {
-      $(".dinoNamesLearned").append("<p>Apatosaurus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[6].name) {
-      $(".dinoNamesLearned").append("<p>Diplodocus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[7].name) {
-      $(".dinoNamesLearned").append("<p>Brachiosaurus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[8].name) {
-      $(".dinoNamesLearned").append("<p>Stegosaurus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[9].name) {
-      $(".dinoNamesLearned").append("<p>Ankylosaurus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[10].name) {
-      $(".dinoNamesLearned").append("<p>Triceratops</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[11].name) {
-      $(".dinoNamesLearned").append("<p>Iguandon</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
-  })
-  $(".memorizedButton").click(function() {
-    if ($(".cardFront h1").text() === dinoCards[12].name) {
-      $(".dinoNamesLearned").append("<p>Parasaurolophus</p>")
-      $("dinosaur").attr("data-dino-index").remove();
-    }
+    dinoIndex = $(this).attr("data-dino-index")
+    var hereIsTheInfoIPromisedYouFunction = dinoCards[dinoIndex]
+    changeCard(hereIsTheInfoIPromisedYouFunction)
   })
 
+  $(".memorizedButton").click(function() {
+      $(".dinoNamesLearned").append("<p>" + dinoCards[dinoIndex].name + "</p>")
+      // $("dinosaur").attr("data-dino-index").remove();
+    })
 //function created to flip the card and shw the answer:
 $(document).ready(function() {
+
   $('.flashCard').on('click', function() {
     $('.flashCard').toggleClass('flipped');
   });
 });
-
 
 });
